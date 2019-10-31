c851aed316cd987128b2e5587748c3df62b3e92b
tlofano <tomilofano@gmail.com>
2019-10-30 21:57:07 -0300

    Htmls autogenerados en la carpeta html

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -3,12 +3,20 @@ import os
 from subprocess import check_call
 from pathlib import Path
 
+HTML_FOLDER_CONTAINER_PATH = "html/"
+
 def post_save(model, os_path, contents_manager):
     """post-save hook for converting notebooks to html scripts"""
     if model['type'] != 'notebook':
         return # only do this for notebooks
+    folder_structure() #Check folder structure
     d, fname = os.path.split(os_path)
     name, ext = os.path.splitext(fname) 
-    check_call(['jupyter', 'nbconvert', '--to', 'html', fname], cwd=d)
+    check_call(['jupyter', 'nbconvert', '--output-dir=' + HTML_FOLDER_CONTAINER_PATH, '--to', 'html', fname], cwd=d)
+
+def folder_structure():
+    if not os.path.isdir(HTML_FOLDER_CONTAINER_PATH):
+        os.mkdir(HTML_FOLDER_CONTAINER_PATH)
+    return True
 
 c.FileContentsManager.post_save_hook = post_save
